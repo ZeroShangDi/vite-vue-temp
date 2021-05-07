fdeaf6166fb6d32e4f5343fdc7327d8b245ca473
上古 <1981914095@qq.com>
2021-05-07 18:24:30 +0800

    chore: 项目目录结构和别名配置

--- docs/chore.md
+++ docs/chore.md
@@ -1,8 +1,24 @@
 # 项目构建
 
+## 项目结构和别名配置
+
+* apis 接口
+* assets 静态文件
+* components 组件库
+* directives 自定义指令
+* ioncs 图标
+* lang 语言
+* layout 布局
+* plugins 插件
+* routes 路由
+* store 状态管理
+* styles 样式
+* utils 工具箱
+* views 页面视图
+
 ## 初始化项目
 ```
-npm init vite-app project-name
+npm init @vitejs/app
 
 cd project-name
 
--- vite2-vue3/vite.config.js
+++ vite2-vue3/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    "@": path.resolve(__dirname, 'src'),
+    "apis": path.resolve(__dirname, 'src/apis'),
+    "comp": path.resolve(__dirname, 'src/components'),
+    "icon": path.resolve(__dirname, 'src/icons'),
+    "lang": path.resolve(__dirname, 'src/lang'),
+    "views": path.resolve(__dirname, 'src/views'),
+    "layout": path.resolve(__dirname, 'src/layout'),
+    "plugins": path.resolve(__dirname, 'src/plugins'),
+    "routes": path.resolve(__dirname, 'src/routes'),
+    "styles": path.resolve(__dirname, 'src/styles'),
+    "store": path.resolve(__dirname, 'src/store'),
+    "utils": path.resolve(__dirname, 'src/utils')
+  },
   plugins: [vue()]
 })
